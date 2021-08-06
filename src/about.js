be10b5426b2b921117e3f7c3576d702ff48f8d1d
atreat738 <atreat738@gmail.com>
2021-08-06 13:35:40 -0500

    Added About page content and stylings

--- src/about.js
+++ src/about.js
@@ -4,10 +4,24 @@ export default function about() {
     content.id = 'content';
     document.body.appendChild(content);
 
+    const aboutContainerDiv = document.createElement('div');
+    aboutContainerDiv.classList.add('aboutContainerDiv');
+    content.appendChild(aboutContainerDiv);
 
-    const h5test = document.createElement('h5');
-    h5test.textContent = 'About Test';
-    content.appendChild(h5test);
+    const imageDiv = document.createElement('div');
+    imageDiv.classList.add('aboutImageDiv');
+    aboutContainerDiv.appendChild(imageDiv);
+
+    const imageCaption = document.createElement('p');
+    imageCaption.classList.add('aboutImageCaption');
+    imageCaption.textContent = 'The Doctor Professor of Coffee: Wyatt';
+    aboutContainerDiv.appendChild(imageCaption);
+
+
+    const aboutText = document.createElement('p');
+    aboutText.classList.add('aboutText');
+    aboutText.textContent = 'Wyatt\'s Coffee was established in 2017 when an ambitious little furball came across a radioactive coffee bean. What followed was only natural. The little furball ate the bean, and was gifted a supreme knowledge over coffee and coffee accessories. This naturally lead Wyatt(the furball) to start a coffee shop catering to humans. As you may have seen on our menu, there are no prices. Please offer appeasement gifts toward the sentient feline and enjoy your drink!';
+    aboutContainerDiv.appendChild(aboutText);
     
     return content;
 }
