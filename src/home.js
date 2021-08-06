d783bcaca54f26f3bfd7417dbefa14686c480ff5
atreat738 <atreat738@gmail.com>
2021-08-06 09:39:45 -0500

    Added Homepage text and stylings

--- src/home.js
+++ src/home.js
@@ -4,9 +4,10 @@ export default function homePage() {
     content.id = 'content';
     document.body.appendChild(content);
 
-    const homeTest = document.createElement('p');
-    homeTest.textContent = 'Wyatt\'s Coffee provides super fast caffeinated services! We are dedicated to customer service, quality products, and delicious drinks! Come on in today for the boost you need!';
-    content.appendChild(homeTest);
-
+    const homeDesc = document.createElement('p');
+    homeDesc.textContent = 'Wyatt\'s Coffee provides super fast caffeinated services! We are dedicated to customer service, quality products, and delicious drinks! Come on in today for the boost you need!';
+    homeDesc.classList.add('homeDesc');
+    content.appendChild(homeDesc);
+    
     return content;
 }
